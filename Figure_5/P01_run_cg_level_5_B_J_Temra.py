df65189915b8991086439fe049b02f26da9e131e
Vincent Friedrich <vfriedrich@informatik.informatik.uni-leipzig.de>
2023-08-01 13:52:53 +0200

    update figure 5

---
+++ Figure_5/P01_run_cg_level_5_B_J_Temra.py
@@ -0,0 +1,24 @@
+import convexgating as cg
+import numpy as np
+import pandas as pd
+import os
+import scanpy as sc
+
+base_data = '/work/users/mh823zote/projects/gating/data/cytof/all/oetjen_all.h5ad'
+level = 'cell_type_lvl5'
+target_folder = '/work/users/mh823zote/projects/gating/data/oetjen/Figure_5/'
+oetjen_all = sc.read_h5ad(base_data)
+samples = ['J','B']
+#samples = ['J','B']
+for sample in samples:
+    oetjen = oetjen_all[oetjen_all.obs['sample'] == sample]
+    sc.pp.subsample(oetjen,n_obs=50000)
+    cts = ['CD4+ TEMRA']
+    if 'not annotated' in cts:
+        cts = [item for item in cts if item != 'not annotated']
+    if not os.path.exists(target_folder + 'P01_' + level + 'Temra_' + sample):
+        os.mkdir(target_folder + 'P01_' + level + 'Temra_' + sample)
+    subpath = os.path.join(target_folder + 'P01_' + level + 'Temra_' + sample, sample)
+    if not os.path.exists(subpath):
+        os.mkdir(subpath)
+    cg.tools.gating_strategy(oetjen,cts,cluster_string = level, save_path = subpath, add_noise=True)
